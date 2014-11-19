f6076df26026ebf7068b27570fffe859d8ff3a49
jorghyq <jorghyq@gmail.com>
2014-11-19 18:06:43 +0100

    test other functions

--- analyzer.py
+++ analyzer.py
@@ -37,13 +37,14 @@ class McAnalyzer:
 		self.latt_len = int(namedata[1])
 		self.num_metal = int(namedata[3])
 		self.nmetal_ind = (self.num_metal - self.nmet_init)/self.nmet_step
-		self.cenergy = int(namedata[4])
+		self.cenergy = float(namedata[4])
 		self.cenergy_ind = (self.cenergy - self.cenergy_init)/self.cernegy_step
 		####### information from the header #######
 		f = open(txt_name, 'r')
 		headdata = f.readline().strip().split(',')
 		f.close()
 		self.cbond_num = float(headdata[0])
+		self.cbond_num_av = self.cbond_num/float(self.num_metal)	
 		self.vbond_num = float(headdata[1])
 		self.total_energy = float(headdata[2])
 		####### information from the matrix #######
@@ -94,7 +95,23 @@ analyzer.set_initial(nmetal_initial,nmetal_step,cenergy_initial,cenergy_step)
 #m1d = np.zeros((ind_metal,ind_cenergy))
 #m2d = np.zeros((ind_metal,ind_cenergy))
 #mdis = np.zeros((ind_metal,ind_cenergy))
+totalenergy = np.zeros((ind_metal,ind_cenergy))
+cbond_num = np.zeros((ind_metal,ind_cenergy))
+vbond_num = np.zeros((ind_metal,ind_cenergy))
+cbond_num_av = np.zeros((ind_metal,ind_cenergy))
 
+for line in files:
+	if line[0] == '1':
+		analyzer.load_txt(line)
+		totalenergy[analyzer.nmetal_ind][analyzer.cenergy_ind] = analyzer.total_energy
+		cbond_num[analyzer.nmetal_ind][analyzer.cenergy_ind] = analyzer.cbond_num
+		vbond_num[analyzer.nmetal_ind][analyzer.cenergy_ind] = analyzer.vbond_num
+		cbond_num_av[analyzer.nmetal_ind][analyzer.cenergy_ind] = analyzer.cbond_num_av
+		
+np.savetxt("totalenergy.txt",totalenergy,delimiter=',')	
+np.savetxt("cbond_num.txt",cbond_num,delimiter=',')
+np.savetxt("vbond_num.txt",vbond_num,delimiter=',')
+np.savetxt("cbond_num_av.txt",cbond_num_av,delimiter=',')
 #print mdense.shape
 #for line in files:
 	#if line[0] == '1':
@@ -132,50 +149,87 @@ analyzer.set_initial(nmetal_initial,nmetal_step,cenergy_initial,cenergy_step)
 #np.savetxt("m1d.txt",m1d, delimiter=',')
 #np.savetxt("m2d.txt",m2d, delimiter=',')
 #np.savetxt("mdis.txt",mdis, delimiter=',')
-mdense = np.loadtxt("mdense.txt", delimiter=',')
-m1d = np.loadtxt("m1d.txt", delimiter=',')
-m2d = np.loadtxt("m2d.txt", delimiter=',')
-mdis = np.loadtxt("mdis.txt", delimiter=',')
-mtotal = mdense + m1d + m2d + mdis
+#mdense = np.loadtxt("mdense.txt", delimiter=',')
+#m1d = np.loadtxt("m1d.txt", delimiter=',')
+#m2d = np.loadtxt("m2d.txt", delimiter=',')
+#mdis = np.loadtxt("mdis.txt", delimiter=',')
+#mtotal = mdense + m1d + m2d + mdis
 
-mdense_p = mdense/mtotal
-m1d_p = m1d/mtotal
-m2d_p = m2d/mtotal
-mdis_p = mdis/mtotal
+#mdense_p = mdense/mtotal
+#m1d_p = m1d/mtotal
+#m2d_p = m2d/mtotal
+#mdis_p = mdis/mtotal
 xmetal = np.array(range(nmetal_initial,nmetal_final + 10,nmetal_step))
-#print xmetal.shape
+##print xmetal.shape
+#fig = plt.figure()
+#fig.add_subplot(2,2,1)
+#for i in range(0,mdense.shape[1]):
+	#plt.plot(xmetal,mdense_p[:,i],label = 'cenerg = %d' % (i*2+1))
+	#plt.text(xmetal[int(ind_metal/2)],mdense_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+#plt.legend(loc=0,fontsize = 8)
+#plt.xlabel('number of metals')
+#plt.ylabel('number of molecules in dense-packed')
+
+
+#fig.add_subplot(2,2,2)
+#for i in range(0,m1d.shape[1]):
+	#plt.plot(xmetal,m1d_p[:,i],label = 'cenerg = %d' % (i*2+1))
+	#plt.text(xmetal[int(ind_metal/2)],m1d_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+#plt.legend(loc=0,fontsize = 8)
+#plt.xlabel('number of metals')
+#plt.ylabel('number of molecules in 1d')
+
+#fig.add_subplot(2,2,3)
+#for i in range(0,m2d.shape[1]):
+	#plt.plot(xmetal,m2d_p[:,i],label = 'cenerg = %d' % (i*2+1))
+	#plt.text(xmetal[int(ind_metal/2)],m2d_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+#plt.legend(loc=0,fontsize = 8)
+#plt.xlabel('number of metals')
+#plt.ylabel('number of molecules in 2d')
+
+#fig.add_subplot(2,2,4)
+#for i in range(0,mdis.shape[1]):
+	#plt.plot(xmetal,mdis_p[:,i],label = 'cenerg = %d' % (i*2+1))
+	#plt.text(xmetal[int(ind_metal/2)],mdis_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+#plt.legend(loc=0,fontsize = 8)
+#plt.xlabel('number of metals')
+#plt.ylabel('number of molecules in disordered')
+#plt.show()
+
+
+
+##################################################################
 fig = plt.figure()
 fig.add_subplot(2,2,1)
-for i in range(0,mdense.shape[1]):
-	plt.plot(xmetal,mdense_p[:,i],label = 'cenerg = %d' % (i*2+1))
-	plt.text(xmetal[int(ind_metal/2)],mdense_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+for i in range(0,totalenergy.shape[1]):
+	plt.plot(xmetal,totalenergy[:,i],label = 'cenerg = %d' % (i*2+1))
+	plt.text(xmetal[int(ind_metal/2)],totalenergy[int(ind_metal/2),i], str(i*2+1),fontsize=8)
 plt.legend(loc=0,fontsize = 8)
 plt.xlabel('number of metals')
-plt.ylabel('number of molecules in dense-packed')
+plt.ylabel('total energy')
 
 
 fig.add_subplot(2,2,2)
-for i in range(0,m1d.shape[1]):
-	plt.plot(xmetal,m1d_p[:,i],label = 'cenerg = %d' % (i*2+1))
-	plt.text(xmetal[int(ind_metal/2)],m1d_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+for i in range(0,cbond_num.shape[1]):
+	plt.plot(xmetal,cbond_num[:,i],label = 'cenerg = %d' % (i*2+1))
+	plt.text(xmetal[int(ind_metal/2)],cbond_num[int(ind_metal/2),i], str(i*2+1),fontsize=8)
 plt.legend(loc=0,fontsize = 8)
 plt.xlabel('number of metals')
-plt.ylabel('number of molecules in 1d')
+plt.ylabel('number of coordination bond')
 
 fig.add_subplot(2,2,3)
-for i in range(0,m2d.shape[1]):
-	plt.plot(xmetal,m2d_p[:,i],label = 'cenerg = %d' % (i*2+1))
-	plt.text(xmetal[int(ind_metal/2)],m2d_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+for i in range(0,vbond_num.shape[1]):
+	plt.plot(xmetal,vbond_num[:,i],label = 'cenerg = %d' % (i*2+1))
+	plt.text(xmetal[int(ind_metal/2)],vbond_num[int(ind_metal/2),i], str(i*2+1),fontsize=8)
 plt.legend(loc=0,fontsize = 8)
 plt.xlabel('number of metals')
-plt.ylabel('number of molecules in 2d')
+plt.ylabel('number of vdW bond')
 
 fig.add_subplot(2,2,4)
-for i in range(0,mdis.shape[1]):
-	plt.plot(xmetal,mdis_p[:,i],label = 'cenerg = %d' % (i*2+1))
-	plt.text(xmetal[int(ind_metal/2)],mdis_p[int(ind_metal/2),i], str(i*2+1),fontsize=8)
+for i in range(0,cbond_num_av.shape[1]):
+	plt.plot(xmetal,cbond_num_av[:,i],label = 'cenerg = %d' % (i*2+1))
+	plt.text(xmetal[int(ind_metal/2)],cbond_num_av[int(ind_metal/2),i], str(i*2+1),fontsize=8)
 plt.legend(loc=0,fontsize = 8)
 plt.xlabel('number of metals')
-plt.ylabel('number of molecules in disordered')
+plt.ylabel('ratio between coordiation bond/number of metals')
 plt.show()
-
