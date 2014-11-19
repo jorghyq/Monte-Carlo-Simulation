05e3685801d79636a707ec361492834027da654a
jorghyq <jorghyq@gmail.com>
2014-11-19 11:44:38 +0100

    main file to analyze the simulation

---
+++ analyzer.py
@@ -0,0 +1,10 @@
+# Python file to analyze the lattice obtained from the simulation with the form specified below
+# the center of the molecules : 3
+# legs : 2
+# metals : 1
+
+import os
+import platform
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
