f5e03012f3c829e88c41e04282816ddb511990d8
jorghyq <jorghyq@gmail.com>
2014-11-26 09:57:31 +0100

    change the x-axis of phase diagram from Ec/Ev to Ev/Ec

--- phase_diagram.py
+++ phase_diagram.py
@@ -9,7 +9,7 @@ import random as rd
 import matplotlib.pyplot as plt
 from PIL import Image
 
-dname = "D:\Dropbox\Project\python\Monte-Carlo-Simulation\\results16\\4-fold-ev-1"
+dname = "D:\Dropbox\Project\python\Monte-Carlo-Simulation\\results13"#\\4-fold-ev-3"
 os.chdir(dname)
 mdense = np.loadtxt("mdense.txt", delimiter=',')
 m1d = np.loadtxt("m1d.txt", delimiter=',')
@@ -27,6 +27,11 @@ print mtotal
 mdense_p = np.flipud(mdense/mtotal)
 m1d_p = np.flipud(m1d/mtotal)
 m2d_p = np.flipud(m2d/mtotal)
+# left and right
+mdense_p = np.fliplr(mdense_p)
+m1d_p = np.fliplr(m1d_p)
+m2d_p = np.fliplr(m2d_p)
+
 #mdense_p = mdense/mtotal
 #m1d_p = m1d/mtotal
 #m2d_p = m2d/mtotal
@@ -46,7 +51,7 @@ im = Image.fromarray(np.uint8(rgb*255.999))
 #print im
 plt.figure()
 #plt.scatter()
-plt.imshow(im,extent=[1,19,0,400],aspect="auto")
-plt.xlabel("Ec/Ev")
+plt.imshow(im,extent=[19/3,1/3,0,550],aspect="auto")
+plt.xlabel("Ev/Ec")
 plt.ylabel("number of metals")
 plt.show()
