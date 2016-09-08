c7a7661c44482c1119d027367fe99178f72c7ba8
jorghyq <jorghyq@gmail.com>
2016-09-08 13:34:08 +0200

    add color for txt2img function

--- txt2img.py
+++ txt2img.py
@@ -13,7 +13,7 @@ os.chdir(dname)
 files = os.listdir(dname)
 
 for line in files:
-    if (line[-4:] == '.txt' and line[0] == '1'):
+    if (line[-4:] == '.txt' and line[0] == '1' and line[-5] != 't'):
         newline = dname+line[:-4]+'.png'
         print line
         print newline
@@ -28,6 +28,8 @@ for line in files:
         temp3, temp4 = np.where(lattice == 10) # mol2
         temp5, temp6 = np.where(lattice == 1)  # metal
         temp7, temp8 = np.where(lattice == 7)
+        temp9, temp10 = np.where(lattice == 2)
+        temp11, temp12 = np.where(lattice == 3)
         # To have a customer designed shape, one has to draw it by himself
         x = [-1.5,-0.5,-0.5,0.5,0.5,1.5,1.5,0.5,0.5,-0.5,-0.5,-1.5,-1.5]
         y = [0.5,0.5,1.5,1.5,0.5,0.5,-0.5,-0.5,-1.5,-1.5,-0.5,-0.5,0.5]
@@ -44,11 +46,12 @@ for line in files:
         ax = plt.axes()
         fig.add_axes(ax)
 
-        ax.scatter(temp1,temp2,s = 120,c = "#0ACEF5",linewidth='0',marker = xy1)
-        ax.scatter(temp3,temp4,s = 120,c = "b",linewidth='0',marker = xy1)
-        ax.scatter(temp7,temp8,s = 20,c = "r",linewidth='0',marker = xy3)
-        ax.scatter(temp5,temp6,s = 20,c = "#F78C00",linewidth='0',marker = "o")
-
+        ax.scatter(temp1,temp2,s = 20,c = "#0ACEF5",linewidth='0',marker = xy1)
+        ax.scatter(temp3,temp4,s = 20,c = "b",linewidth='0',marker = xy1)
+        ax.scatter(temp7,temp8,s = 2,c = "r",linewidth='0',marker = xy3)
+        ax.scatter(temp5,temp6,s = 2,c = "#F78C00",linewidth='0',marker = "o")
+        ax.scatter(temp9,temp10,s = 2,c = "g",linewidth='0',marker = "o")
+        ax.scatter(temp11,temp12,s = 2,c = "m",linewidth='0',marker = "o")
         plt.xlim([-0.5, latt_len-0.5])
         plt.ylim([-0.5, latt_len-0.5])
 
