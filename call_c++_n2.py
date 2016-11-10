e0c9c59b7cd196b3b83d51e4302f0bbd49eef253
jorghyq <jorghyq@gmail.com>
2016-11-10 12:53:32 +0100

    fixed call_n2 function

--- call_c++_n2.py
+++ call_c++_n2.py
@@ -13,63 +13,70 @@ import time
 import json
 
 total_run = 1000000
-num_mol1 = 100
-num_mol2 = 0
-num_metal = 0
-cenergy = 40
-venergy = 5
-mcenergy = 40
 ffn=100
 latt_len = 100
 restore = 1
 
 #print total_run, num_mol1, num_mol2, num_metal
-cenergy = 40
-cenergy_init = cenergy
+cenergy_init = 40
+cenergy = cenergy_init
 cenergy_max = 40
 cenergy_step = 1
 
-mcenergy = 40
-mcenergy_init = mcenergy
-mcenergy_max =
-mcenergy_step =
+mcenergy_init = 40
+mcenergy = mcenergy_init
+mcenergy_max = 40
+mcenergy_step = 1
 
-num_mol1 = 100
-num_mol1_init = num_mol1
-num_mol1_max = 100
+venergy_init = 5
+venergy = venergy_init
+venergy_max = 5
+venergy_step = 1
+
+num_mol1_init = 150
+num_mol1 = num_mol1_init
+num_mol1_max = 150
 num_mol1_step = 1
 
-num_mol2 = 100
-num_mol2_init = num_mol2
-num_mol2_max = 100
-num_mol2_step = 1
+num_mol2_init = 50
+num_mol2 = num_mol2_init
+num_mol2_max = 200
+num_mol2_step = 50
 
-num_metal = 50
-num_metal_init = num_metal
+num_metal_init = 50
+num_metal = num_metal_init
 num_metal_max = 300
 num_metal_step = 50
 
-while cenergy <= cenergy_max:
-    while mcenergy <= mcenergy_max:
-        while num_mol1 <= num_mol1_max:
-            while num_mol2 <= num_mol2_max:
-                while num_metal <= num_metal_max
-                    command = './mc-rect-lattice-func-linuxr6 -a %d -b %d -c %d -d %d -e %f -f %f -g %f -h %d -i %d' % (total_run,num_mol1,num_mol2,num_metal,cenergy,venergy,mcenergy,ffn,restore)
-                    args = shlex.split(command)
-                    print args
-                    os.system(command)
-                    num_metal = num_metal + num_metal_step
-               num_metal = num_metal_init
-               num_mol2 = num_mol2 + num_mol2_step
+while venergy <= venergy_max:
+    while cenergy <= cenergy_max:
+        while mcenergy <= mcenergy_max:
+            while num_mol1 <= num_mol1_max:
+                while num_mol2 <= num_mol2_max:
+                    while num_metal <= num_metal_max:
+                        command = './mc-rect-lattice-func-linux6 -a %d -b %d -c %d -d %d -e %f -f %f -g %f -h %d -i %d' % (total_run,num_mol1,num_mol2,num_metal,cenergy,venergy,mcenergy,ffn,restore)
+                        args = shlex.split(command)
+                        #print args
+                        os.system(command)
+                        num_metal = num_metal + num_metal_step
+                        #print venergy, cenergy, mcenergy, num_mol1, num_mol2, num_metal
+                    num_metal = num_metal_init
+                    num_mol2 = num_mol2 + num_mol2_step
+                num_metal = num_metal_init
+                num_mol2 = num_mol2_init
+                num_mol1 = num_mol1 + num_mol1_step
             num_metal = num_metal_init
             num_mol2 = num_mol2_init
-            num_mol1 = num_mol1 + num_mol1_step
+            num_mol1 = num_mol1_init
+            mcenergy = mcenergy + mcenergy_step
         num_metal = num_metal_init
         num_mol2 = num_mol2_init
         num_mol1 = num_mol1_init
-        mcenergy = mcenergy + mcenergy_step
+        mcenergy = mcenergy_init
+        cenergy = cenergy + cenergy_step
     num_metal = num_metal_init
     num_mol2 = num_mol2_init
     num_mol1 = num_mol1_init
     mcenergy = mcenergy_init
-    cenergy = cenergy + cenergy_step
+    cenergy = cenergy_init
+    venergy = venergy + venergy_step
